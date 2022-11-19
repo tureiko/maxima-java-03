b36cd85e96fb3a048472311f868f889d0db177d9
deemon1988 <dmn72835@gmail.com>
2022-11-19 23:14:43 +0300

    Фабрика котов

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,11 +4,26 @@ package org.example;
 
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         Birthday birthday = new Birthday();
         birthday.getAge(1988,12,19);
         birthday.nextBirthday(1988,12,19);
+        System.out.println();
+
+        try {
+            Cat cat = new Cat("Барсик", -1, false);
+        }
+       catch (IncorrectCatWeightException ex) {
+           System.out.println("Неправильный кот");
+       }
+
+
+  Cat cat = CatFactory.createCat("Мурзик", 4);
+        System.out.println(cat.getWeight());
+        System.out.println(cat.isAngry());
+
+
 
     }
 }
---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Cat {
+    private String name;
+    private int weight;
+    private boolean isAngry;
+
+    public Cat(String name, int weight, boolean isAngry) throws Exception {
+        if (weight <0) {
+            throw new IncorrectCatWeightException("Неправильный кот");
+        }
+        this.name = name;
+        this.weight = weight;
+        this.isAngry = isAngry;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isAngry() {
+        return isAngry;
+    }
+
+    public void setAngry(boolean angry) {
+        isAngry = angry;
+    }
+}
