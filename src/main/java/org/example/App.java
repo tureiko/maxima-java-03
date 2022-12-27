8fc05929536016152bb00a5c7cef9c3313645201
tureiko <antonina03032007>
2022-12-27 22:27:00 +0300

    д.з. Birthday

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,12 @@
 package org.example;
 
-
-import java.time.Month;
-
 public class App
 {
-    public static void main( String[] args )
-    {Birthday birthday=new Birthday();
-       System.out.println(birthday.getAge(1983, Month.SEPTEMBER,11));
+    public static void main( String[] args ){
+     Birthday birthday = new Birthday();
+        birthday.getAge(1983,9,11);
+        birthday.nextBirthday(1983,9,11);
+        System.out.println();
 
     }
 }
--- src/main/java/org/example/Birthday.java
+++ src/main/java/org/example/Birthday.java
@@ -2,22 +2,47 @@ package org.example;
 
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.temporal.ChronoUnit;
 
 public class Birthday {
 
-   public static int getAge(int year, Month month, int date){
-    LocalDate datee=LocalDate.of(1983, Month.SEPTEMBER,11);
-    int days=0;
-    LocalDate daten=LocalDate.now();
-    days= (int) ChronoUnit.DAYS.between(datee,daten);
-    return days;
+    int getAge(int year,int month, int date) {
+        LocalDate birthDay = LocalDate.of(year, month,date);
+        LocalDate nowData = LocalDate.now();
+        int inDays = (int) ChronoUnit.DAYS.between(birthDay, nowData);
+        System.out.println("Возвраст в днях "+inDays);
 
-}
-public static LocalDate nextBirthday(int year, Month month, int date){
-   nextBirthday(1983,Month.SEPTEMBER,11).plus(1000,ChronoUnit.DAYS);
- return ;
-}
+        LocalDate birthDay1=birthDay;
 
+        int years = birthDay.getYear();
+        int yearsNow = nowData.getYear();
+        int inDays1;
+
+        for (int i = years; i<yearsNow; i++) { birthDay1=birthDay1.plus(1,ChronoUnit.YEARS); }
+        if (birthDay1.isAfter(nowData))
+        {
+            birthDay1=birthDay1.minus(1,ChronoUnit.YEARS);
+            inDays1 = (int) ChronoUnit.DAYS.between(birthDay, birthDay1);
+        }
+        else {  inDays1 = (int) ChronoUnit.DAYS.between(birthDay, birthDay1);}
+        System.out.println("Возвраст в днях для полных лет "+inDays1);
+
+        return inDays;
+    }
+
+    LocalDate nextBirthday(int year, int month, int date)
+    {
+
+        LocalDate nextBirthday = LocalDate.of(year, month, date);
+        LocalDate dat = LocalDate.now();
+
+        while (nextBirthday.isBefore(dat))
+        { nextBirthday = nextBirthday.plusDays(1000);}
+
+
+
+        System.out.println("Следующий день рождения в 1000 дней "+nextBirthday);
+
+        return nextBirthday;
+    }
 }
