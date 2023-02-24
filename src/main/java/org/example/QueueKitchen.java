f88f28077b8911fdfdd7c70cbef72bea77a6fbce
Роман <112479015+tureiko@users.noreply.github.com>
2023-02-24 20:57:08 +0300

    Update QueueKitchen.java

--- src/main/java/org/example/QueueKitchen.java
+++ src/main/java/org/example/QueueKitchen.java
@@ -16,7 +16,6 @@ public class QueueKitchen<T> implements AnimalKitchen<T> {
     @Override
     public void add(T arg) {
         getAnimals().add(arg);
-        // System.out.println(getAnimals());
     }
 
     @Override
@@ -25,7 +24,6 @@ public class QueueKitchen<T> implements AnimalKitchen<T> {
         for (int i = 0; i <  getAnimals().size(); i++) {
             while (! getAnimals().isEmpty()) {
                 getAnimals().remove(i);
-                // System.out.println ( getAnimals().remove(i)+" "+ getAnimals().size());
             }
 
         }
